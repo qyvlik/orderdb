abed9b53e97c7189ed23b9ea195c3d204d666155
qyvlik <qyvlik@qq.com>
2018-12-09 20:15:40 +0800

    fix code

--- src/main/java/io/github/qyvlik/orderdb/config/OrderDBConfig.java
+++ src/main/java/io/github/qyvlik/orderdb/config/OrderDBConfig.java
@@ -1,21 +1,16 @@
 package io.github.qyvlik.orderdb.config;
 
-import io.github.qyvlik.jsonrpclite.core.common.ITaskQueue;
 import org.iq80.leveldb.DB;
 import org.iq80.leveldb.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.File;
-import java.util.List;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.iq80.leveldb.impl.Iq80DBFactory.factory;
@@ -26,7 +21,7 @@ public class OrderDBConfig {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Value("${orderdb.directory}")
-    private String levelDBDirectory;
+    private String orderDBDirectory;
 
     private DB levelDB;
 
@@ -36,7 +31,7 @@ public class OrderDBConfig {
         options.createIfMissing(true);
         DB db = null;
         try {
-            db = factory.open(new File(levelDBDirectory), options);
+            db = factory.open(new File(orderDBDirectory), options);
         } catch (Exception e) {
             logger.error("create leveldb failure:", e);
             throw new RuntimeException(e);
